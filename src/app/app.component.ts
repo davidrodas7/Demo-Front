12e429f2e7394f49c0f3ec0ab96e24df0a596e46
David Rodas <_David.Rodas.EXT.AZ@sodexo.com>
2025-03-18 11:52:41 -0500

    employee-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'employee-app';
+  title = 'hola mundo';
+  enabled = false;
+
+  constructor() { }
+
+  setEnabled() {
+    this.enabled = true;
+  }
 }
